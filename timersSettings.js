96c3175cb8f3d6430fcd5886d592633a748e6b4c
Lukas Hermann <hey@lukashermann.dev>
2022-11-22 12:48:36 +0300

    refactor: settings => timersSettings

--- timersSettings.js
+++ timersSettings.js
@@ -2,10 +2,8 @@ import { CONTINUE } from './timerBehaviors.js'
 import { HHMMSS } from './countdownFormats.js'
 import { AUTO } from './todFormats.js'
 
-export const timersSettings = {
+export default {
   countdownFormat: { type: String, default: HHMMSS },
   todFormat: { type: String, default: AUTO },
   behavior: { type: String, default: CONTINUE },
 }
-
-export default timersSettings
