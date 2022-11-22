87230a8705e1fafc7d614c2a86d7c31574f448fe
Lukas Hermann <hey@lukashermann.dev>
2022-11-22 16:08:07 +0300

    fix: timersSettings export

--- timersSettings.js
+++ timersSettings.js
@@ -2,8 +2,12 @@ import { CONTINUE } from './timerBehaviors.js'
 import { HHMMSS } from './countdownFormats.js'
 import { AUTO } from './todFormats.js'
 
+export const countdownFormat = { type: String, default: HHMMSS }
+export const todFormat = { type: String, default: AUTO }
+export const behavior = { type: String, default: CONTINUE }
+
 export default {
-  countdownFormat: { type: String, default: HHMMSS },
-  todFormat: { type: String, default: AUTO },
-  behavior: { type: String, default: CONTINUE },
+  countdownFormat,
+  todFormat,
+  behavior,
 }
