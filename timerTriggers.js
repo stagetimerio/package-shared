f12ed2919d380a009990da38dce319709c08b753
Lukas Hermann <hey@lukashermann.dev>
2023-06-26 17:27:45 +0200

    chore: remove obsolete const

--- timerTriggers.js
+++ timerTriggers.js
@@ -1,8 +1,7 @@
 export const MANUAL = 'MANUAL'
 export const LINKED = 'LINKED'
 export const SCHEDULED = 'SCHEDULED'
-export const UNTIL = 'UNTIL'
 
-export const triggers = { MANUAL, LINKED, SCHEDULED, UNTIL }
+export const triggers = { MANUAL, LINKED, SCHEDULED }
 
 export default { ...triggers }
