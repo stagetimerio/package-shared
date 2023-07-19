81d93651b1729443b6256e16abddc091d51a972f
Lukas Hermann <hey@lukashermann.dev>
2023-07-19 21:05:49 +0200

    feat: add SHIPBELL and SHIPHORN chimes

--- timerChimes.js
+++ timerChimes.js
@@ -5,9 +5,11 @@ export const BEEP = 'BEEP'
 export const BUZZER = 'BUZZER'
 export const GONG = 'GONG'
 export const WINDCHIME = 'WINDCHIME'
+export const SHIPBELL = 'SHIPBELL'
+export const SHIPHORN = 'SHIPHORN'
 export const ITSTIME = 'ITSTIME'
 export const SILENCE = 'SILENCE'
 
-export const chimes = { NONE, BELL, DING, WINDCHIME, BEEP, BUZZER, GONG, ITSTIME, SILENCE }
+export const chimes = { NONE, BELL, DING, WINDCHIME, BEEP, BUZZER, GONG, SHIPBELL, SHIPHORN, ITSTIME, SILENCE }
 
 export default { ...chimes }
