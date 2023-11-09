7cefc452be65ce939d75641a9e9d8e1434bde1a0
Lukas Hermann <hey@lukashermann.dev>
2023-11-09 23:00:27 +0100

    feat: remove unnecessary export

--- teamRoles.js
+++ teamRoles.js
@@ -1,7 +1,3 @@
 export const TEAM_OWNER = 'TEAM_OWNER' // All team permissions
 export const TEAM_BILLING = 'TEAM_BILLING' // View/Edit billing information
 export const TEAM_MEMBER = 'TEAM_MEMBER' // View/Edit/Create/Delete rooms
-
-export const roles = { TEAM_OWNER, TEAM_BILLING, TEAM_MEMBER }
-
-export default { ...roles }
