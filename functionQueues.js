20c84cf9806b5c22c8e92e04397f1dd25458017a
Lukas Hermann <hey@lukashermann.dev>
2023-11-09 23:00:56 +0100

    feat: add new PubSub function-queue key

--- functionQueues.js
+++ functionQueues.js
@@ -1,5 +1,6 @@
 export const QUEUE_EMAIL = 'functions-queue-email'
 export const QUEUE_AIRTABLE = 'functions-queue-airtable'
+export const QUEUE_LICENSE_KEY = 'functions-queue-license-key'
 export const QUEUE_STATS = 'functions-queue-stats'
 export const QUEUE_DATA = 'functions-queue-data'
 export const QUEUE_CLEANUP = 'functions-queue-cleanup'
