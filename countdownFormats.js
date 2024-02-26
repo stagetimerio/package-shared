91144956177524053d922c5a01e534120f9fa5da
Lukas Hermann <hey@lukashermann.dev>
2024-02-26 22:07:40 +0100

    feat: add more countdown formats

--- countdownFormats.js
+++ countdownFormats.js
@@ -1,12 +1,15 @@
 export const DHHMMSS = 'DHHMMSS'
 export const DHHMMSSF = 'DHHMMSSF'
-export const HHHMMSS = 'HHHMMSS' // HHMMSS
-export const HHHMMSSF = 'HHHMMSSF' // HHMMSSF
+export const HHHMMSS = 'HHHMMSS'
+export const HHHMMSSF = 'HHHMMSSF'
 export const MMMSS = 'MMMSS'
 export const MMMSSF = 'MMMSSF'
 export const SSS = 'SSS'
 export const SSSF = 'SSSF'
-export const L_DHMS = 'L_DHMS' // DHMS
+export const L_D = 'L_D'
+export const L_DH = 'L_DH'
+export const L_DHM = 'L_DHM'
+export const L_DHMS = 'L_DHMS'
 export const L_HMS = 'L_HMS'
 export const L_MS = 'L_MS'
 export const L_S = 'L_S'
@@ -14,7 +17,7 @@ export const L_S = 'L_S'
 // Legacy
 export const HHMMSS = 'HHMMSS'
 
-export const countdownFormats = { DHHMMSS, DHHMMSSF, HHHMMSS, HHHMMSSF, MMMSS, MMMSSF, SSS, SSSF, L_DHMS, L_HMS, L_MS, L_S }
+export const countdownFormats = { DHHMMSS, DHHMMSSF, HHHMMSS, HHHMMSSF, MMMSS, MMMSSF, SSS, SSSF, L_D, L_DH, L_DHM, L_DHMS, L_HMS, L_MS, L_S }
 export const DEFAULT_COUNTDOWN_FORMAT = HHHMMSS
 
 export default {
