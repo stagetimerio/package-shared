2a165917324d21600e9c088e3e286a351f77008e
Lukas Hermann <hey@lukashermann.dev>
2024-05-27 15:26:44 +0200

    feat: add new chiphorn chime

--- timerChimes.js
+++ timerChimes.js
@@ -6,10 +6,11 @@ export const BUZZER = 'BUZZER'
 export const GONG = 'GONG'
 export const WINDCHIME = 'WINDCHIME'
 export const SHIPBELL = 'SHIPBELL'
-export const SHIPHORN = 'SHIPHORN'
+export const SHIPHORNSINGLE = 'SHIPHORNSINGLE'
+export const SHIPHORNDOUBLE = 'SHIPHORN' // for legacy reasons
 export const ITSTIME = 'ITSTIME'
 export const SILENCE = 'SILENCE'
 
-export const chimes = { NONE, BELL, DING, WINDCHIME, BEEP, BUZZER, GONG, SHIPBELL, SHIPHORN, ITSTIME, SILENCE }
+export const chimes = { NONE, BELL, DING, WINDCHIME, BEEP, BUZZER, GONG, SHIPBELL, SHIPHORNSINGLE, SHIPHORNDOUBLE, ITSTIME, SILENCE }
 
 export default { ...chimes }
