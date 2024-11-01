381e9920d00bff0676357bf2198fc8c57e7fdba0
Lukas Hermann <hey@lukashermann.dev>
2024-11-01 21:02:52 +0100

    feat: add timer type: TIME_WARP

--- timerTypes.js
+++ timerTypes.js
@@ -1,6 +1,7 @@
 export const DURATION = 'DURATION'
 export const FINISH_TIME = 'FINISH_TIME'
+export const TIME_WARP = 'TIME_WARP'
 
-export const types = { DURATION, FINISH_TIME }
+export const types = { DURATION, FINISH_TIME, TIME_WARP }
 
 export default { ...types }
