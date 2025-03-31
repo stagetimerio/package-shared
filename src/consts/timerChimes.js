67b2be4fe084640d62957f3a7c0aa71559a878ee
Lukas Hermann <hey@lukashermann.dev>
2025-03-31 19:03:25 +0200

    feat: update list of available chimes

--- src/consts/timerChimes.js
+++ src/consts/timerChimes.js
@@ -1,16 +1,18 @@
 export const NONE = 'NONE'
-export const BELL = 'BELL'
-export const DING = 'DING'
-export const BEEP = 'BEEP'
+export const BELL1X = 'BELL' // for legacy reasons
+export const BELL2X = 'BELL2X'
+export const BELL3X = 'BELL3X'
+export const DING1X = 'DING' // for legacy reasons
+export const DING2X = 'DING2X'
+export const DING3X = 'DING3X'
+export const BEEP1X = 'BEEP1X'
+export const BEEP2X = 'BEEP2X'
+export const BEEP3X = 'BEEP' // for legacy reasons
 export const BUZZER = 'BUZZER'
 export const GONG = 'GONG'
 export const WINDCHIME = 'WINDCHIME'
 export const SHIPBELL = 'SHIPBELL'
-export const SHIPHORNSINGLE = 'SHIPHORNSINGLE'
-export const SHIPHORNDOUBLE = 'SHIPHORN' // for legacy reasons
+export const SHIPHORN1X = 'SHIPHORNSINGLE'
+export const SHIPHORN2X = 'SHIPHORN' // for legacy reasons
 export const ITSTIME = 'ITSTIME'
 export const SILENCE = 'SILENCE'
-
-export const chimes = { NONE, BELL, DING, WINDCHIME, BEEP, BUZZER, GONG, SHIPBELL, SHIPHORNSINGLE, SHIPHORNDOUBLE, ITSTIME, SILENCE }
-
-export default { ...chimes }
