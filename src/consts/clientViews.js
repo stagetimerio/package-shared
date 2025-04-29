e9ffe9530c34d1b25a92cebcff5cc33a0a88dce1
Lukas Hermann <hey@lukashermann.dev>
2025-04-28 21:25:32 -0300

    feat: add client view operator

--- src/consts/clientViews.js
+++ src/consts/clientViews.js
@@ -2,8 +2,9 @@ export const VIEWER = 'VIEWER'
 export const CONTROLLER = 'CONTROLLER'
 export const AGENDA = 'AGENDA'
 export const MODERATOR = 'MODERATOR'
+export const OPERATOR = 'OPERATOR'
 export const ANONYMOUS = 'ANONYMOUS'
 
-export const views = { VIEWER, CONTROLLER, AGENDA, MODERATOR, ANONYMOUS }
+export const views = { VIEWER, CONTROLLER, AGENDA, MODERATOR, OPERATOR, ANONYMOUS }
 
 export default { ...views }
